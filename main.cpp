ba8ff9cd7c2114b52f5a75225c75146f88b5e8a7
Alexandr Kutuzov <alleteam@gmail.com>
2013-01-10 17:42:02 +0800

    change qt plugin directory in runtime

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <QApplication>
+#include <QDir>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+#ifndef DEBUG_BUILD
+    #if MAC_OS_X_VERSION_10_5
+    QDir dir(QApplication::applicationDirPath());
+    dir.cdUp();
+    dir.cd("plugins");
+    QApplication::setLibraryPaths(QStringList(dir.absolutePath()));
+    #elif WINNT
+    QDir dir(QApplication::applicationDirPath());
+    dir.cd("plugins");
+    QApplication::setLibraryPaths(QStringList(dir.absolutePath()));
+    #endif
+#endif
     QApplication a(argc, argv);
     MainWindow w(&a);
     w.show();
